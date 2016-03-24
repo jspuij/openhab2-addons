61440e5eed6cb66e89736cf16616e61da5fce335
Jan-Willem Spuij <jws@birdseyeview.nl>
2016-03-24 13:40:40 +0100

    Added gateway + parameter for port.

--- addons/binding/org.openhab.binding.openthermgateway/src/main/java/org/openhab/binding/openthermgateway/handler/OpenThermGatewayHandler.java
+++ addons/binding/org.openhab.binding.openthermgateway/src/main/java/org/openhab/binding/openthermgateway/handler/OpenThermGatewayHandler.java
@@ -7,7 +7,7 @@
  */
 package org.openhab.binding.openthermgateway.handler;
 
-import static org.openhab.binding.openthermgateway.OpenThermGatewayBindingConstants.*;
+import static org.openhab.binding.openthermgateway.OpenThermGatewayBindingConstants.CHANNEL_GATEWAY_VERSION;
 
 import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.Thing;
@@ -19,21 +19,21 @@ import org.slf4j.LoggerFactory;
 /**
  * The {@link OpenThermGatewayHandler} is responsible for handling commands, which are
  * sent to one of the channels.
- * 
+ *
  * @author Jan-Willem Spuij - Initial contribution
  */
 public class OpenThermGatewayHandler extends BaseThingHandler {
 
     private Logger logger = LoggerFactory.getLogger(OpenThermGatewayHandler.class);
 
-	public OpenThermGatewayHandler(Thing thing) {
-		super(thing);
-	}
+    public OpenThermGatewayHandler(Thing thing) {
+        super(thing);
+    }
 
-	@Override
-	public void handleCommand(ChannelUID channelUID, Command command) {
-        if(channelUID.getId().equals(CHANNEL_1)) {
+    @Override
+    public void handleCommand(ChannelUID channelUID, Command command) {
+        if (channelUID.getId().equals(CHANNEL_GATEWAY_VERSION)) {
             // TODO: handle command
         }
-	}
+    }
 }
--- addons/binding/org.openhab.binding.openthermgateway/src/main/java/org/openhab/binding/openthermgateway/internal/OpenThermGatewayHandlerFactory.java
+++ addons/binding/org.openhab.binding.openthermgateway/src/main/java/org/openhab/binding/openthermgateway/internal/OpenThermGatewayHandlerFactory.java
@@ -7,46 +7,47 @@
  */
 package org.openhab.binding.openthermgateway.internal;
 
-import static org.openhab.binding.openthermgateway.OpenThermGatewayBindingConstants.*;
+import static org.openhab.binding.openthermgateway.OpenThermGatewayBindingConstants.THING_TYPE_GATEWAY;
 
 import java.util.Collections;
 import java.util.Set;
 
-import org.openhab.binding.openthermgateway.handler.OpenThermGatewayHandler;
-
-import org.eclipse.smarthome.config.core.Configuration;
-import org.eclipse.smarthome.core.thing.Bridge;
 import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.ThingTypeUID;
-import org.eclipse.smarthome.core.thing.ThingUID;
 import org.eclipse.smarthome.core.thing.binding.BaseThingHandlerFactory;
 import org.eclipse.smarthome.core.thing.binding.ThingHandler;
+import org.openhab.binding.openthermgateway.handler.OpenThermGatewayHandler;
 
 /**
- * The {@link OpenThermGatewayHandlerFactory} is responsible for creating things and thing 
+ * The {@link OpenThermGatewayHandlerFactory} is responsible for creating things and thing
  * handlers.
- * 
+ *
  * @author Jan-Willem Spuij - Initial contribution
  */
 public class OpenThermGatewayHandlerFactory extends BaseThingHandlerFactory {
-    
-    private final static Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Collections.singleton(THING_TYPE_SAMPLE);
-    
+
+    private final static Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Collections.singleton(THING_TYPE_GATEWAY);
+
     @Override
     public boolean supportsThingType(ThingTypeUID thingTypeUID) {
         return SUPPORTED_THING_TYPES_UIDS.contains(thingTypeUID);
     }
 
+    /**
+     * Creates a handler for the specified thing.
+     *
+     * @param thing a thing.
+     * @return a thing handler for the thing.
+     */
     @Override
     protected ThingHandler createHandler(Thing thing) {
 
         ThingTypeUID thingTypeUID = thing.getThingTypeUID();
 
-        if (thingTypeUID.equals(THING_TYPE_SAMPLE)) {
+        if (thingTypeUID.equals(THING_TYPE_GATEWAY)) {
             return new OpenThermGatewayHandler(thing);
         }
 
         return null;
     }
 }
-
